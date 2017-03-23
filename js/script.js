ef407ebbcde50e15d97e6c07d3f8c2e831273a77
bb00 <shadowninja.cp@gmail.com>
2017-03-23 10:21:58 -0400

    Try to fix the setColor parameter as a jQuery Selector

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@ var color = ["red", "blue", "black"];
 var i = 0;
 $(document).ready(function () {
     $("p").click(function () {
-        setColor(this);
+        setColor($(this));
     })
 });
 function setColor(obj) {
-    obj.css("color", color[i].toString());
+
     if(i < 2) {
         i++;
     } else {
         i = 0;
     }
+    obj.css("color", color[i].toString());
     return color[i];
 }
